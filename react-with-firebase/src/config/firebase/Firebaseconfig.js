705013c162583f7e21617b344a174915be015618
muhammadhussainharoon <hussain356q@gmail.com>
2024-01-15 03:13:05 +0500

    process

---
+++ react-with-firebase/src/config/firebase/Firebaseconfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQZ2XOYC0cpWcBD1BHzIi9ZoTHMQYTPs4",
+  authDomain: "login-register-practice-7183e.firebaseapp.com",
+  projectId: "login-register-practice-7183e",
+  storageBucket: "login-register-practice-7183e.appspot.com",
+  messagingSenderId: "952785359555",
+  appId: "1:952785359555:web:49f34709b6488d09e4c876",
+  measurementId: "G-7HRK1TKZWB"
+};
+
+
+const app = initializeApp(firebaseConfig);
